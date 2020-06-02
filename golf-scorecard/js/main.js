9863f35564c89139e1408a738caf56ad8a351238
LukeGiles3 <lukegiles@Lukes-MacBook-Pro.local>
2020-06-02 08:35:14 -0600

    in, out, tot scores plus highlights

--- golf-scorecard/js/main.js
+++ golf-scorecard/js/main.js
@@ -43,17 +43,24 @@ function getCourse(id) {
         holesHtml += 
         `<div id="${num}">
         <p class="table">${increment()}</p>
-        <p class="table">${hole.teeBoxes[teeBox].yards}</p>
-        <p class="table">${hole.teeBoxes[teeBox].par}</p>
-        <p class="table">${hole.teeBoxes[teeBox].hcp}</p>
-        <input id="input${num}" class="table"></input>
+        <p id="yards" class="table">${hole.teeBoxes[teeBox].yards}</p>
+        <p id="par" class="table">${hole.teeBoxes[teeBox].par}</p>
+        <p id="hcp" class="table">${hole.teeBoxes[teeBox].hcp}</p>
         </div>`
       })
       document.getElementById('holes').innerHTML = holesHtml;
 
-      $("#8").after(`<div id="out"><p class="table">OUT</p><p class="table">1</p><p class="table">1</p><p class="table">1</p></div>`)
-
-
+      let parOut = holes.filter(val => val.hole < 10).reduce((acc, val) => { return acc += val.teeBoxes[0].par}, 0)
+      parIn = holes.filter(val => val.hole >= 10).reduce((acc, val) => { return acc += val.teeBoxes[0].par}, 0)
+      parTot = parIn + parOut
+      yardOut = holes.filter(val => val.hole < 10).reduce((acc, val) => { return acc += val.teeBoxes[0].yards}, 0)
+      yardIn = holes.filter(val => val.hole >= 10).reduce((acc, val) => { return acc += val.teeBoxes[0].yards}, 0)
+      yardTot = yardOut + yardIn
+      
+      $("#8").after(`<div id="out"><p class="table-out">OUT</p><p class="table-out">${yardOut}</p><p class="table-out">${parOut}</p><p class="table-out">*</p></div>`)
+      // $("#out").after(`<div id="int"><p class="table-input">INT</p><input class="table-input"></input><input class="table-input"></input><input class="table-input"></input></div>`)
+      $("#17").after(`<div id="in"><p class="table-out">IN</p><p class="table-out">${yardIn}</p><p class="table-out">${parIn}</p><p class="table-out">*</p></div>`)
+      $("#in").after(`<div id="total"><p class="table-out">TOT</p><p class="table-out">${yardTot}</p><p class="table-out">${parTot}</p><p class="table-out">*</p></div>`)
       });
     }
   );
